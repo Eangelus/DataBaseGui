85f9cab83b2d8ffa834926803a496f27b5d587d0
Eangelus <bernecker.thomas@gmx.de>
2022-05-09 14:09:38 +0200

    Initial commit first Commit from IDE ^^

---
+++ MyDataBaseBot.py
@@ -0,0 +1,91 @@
+import mysql.connector
+
+
+class MyDataBaseBot:
+
+    def __init__(self, host, port, username, password, database):
+        self.host = host
+        self.port = int(port)
+        self.username = username
+        self.nameDataBase = database
+        self.password = password
+        self.base = mysql.connector.connect(host=self.host, port=self.port, user=self.username, password=self.password, database=self.nameDataBase)
+        print('connect done')
+        self.bot_cursor = self.base.cursor()
+        self.tablename = ""
+        #self.bot_cursor.execute("SHOW DATABASES")
+        #for db in self.bot_cursor:
+            #print(db)
+        self.spaltenname = ""
+        self.vari = ""
+
+        self.id = ""
+        self.auswahl = ""
+
+    def use(self, dataBaseName):
+        self.bot_cursor.execute("USE " + dataBaseName)
+
+    def create_bases(self, nameDataBase):
+        self.bot_cursor.execute("CREATE DATABASES " + nameDataBase)
+
+    def einf(self, tablename, spaltenname, vari):
+        self.bot_cursor.execute("INSERT INTO " + tablename + " SET " + spaltenname + " = " + vari)
+
+    def update(self, tablename, spaltenname, vari, id, auswahl):
+        self.bot_cursor.execute("UPDATE "+tablename+" SET "+spaltenname+" = "+vari+" WHERE "+id+" = "+auswahl)
+
+    def zeige(self, tablename):
+        self.bot_cursor.execute("SELECT * FROM " + tablename)
+        r = self.bot_cursor.fetchall()
+        return r
+
+    def enf(self, tablename, auswahl):
+        self.bot_cursor.execute("DELETE FROM " + tablename + " WHERE id_eintrag=" + str(auswahl))
+        print("LEckmichamarsch bot")
+
+
+
+    def zeige_base(self):
+        self.bot_cursor.execute("SHOW DATABASES")
+        resulte = self.bot_cursor.fetchall()
+        return resulte
+
+    def zeige_tables(self):
+        self.bot_cursor.execute("SHOW TABLES")
+        r = self.bot_cursor.fetchall()
+        return r
+
+    def zeige_struk(self, tablename):
+        self.bot_cursor.execute("Desc " + tablename)
+        result = self.bot_cursor.fetchall()
+        return result
+
+    def delete_table(self):
+        self.bot_cursor.execute()
+
+    def join_left(self):
+        self.bot_cursor.execute()
+
+    def con_close(self):
+        self.bot_cursor.close()
+        self.base.close()
+
+    #---------------------------------------- geter / setter -------------------------------
+    def get_tablename(self):
+        return self.tablename
+
+    def set_tablename(self, tablename):
+        self.tablename = tablename
+
+
+    def set_spaltenname(self,spaltenname):
+        self.spaltenname = spaltenname
+    def get_spaltenname(self, spaltenname):
+        return self.spaltenname
+
+        """
+        self.vari = ""
+        self.nameDataBase = ""
+        self.id = ""
+        self.auswahl = ""
+"""
---
+++ win.py
@@ -0,0 +1,140 @@
+import datetime
+
+import gui.GuiMain
+from gui.GuiMain import *
+import MyDataBaseBot
+
+class Mymain(Ui_MainWindow):
+
+    #construktor
+    def __init__(self):
+        super().__init__(MainWindow)
+        self.bot = MyDataBaseBot.MyDataBaseBot("127.0.0.1", 3306, "root", "", "todobase")
+        self.ltloadButton.clicked.connect(self.pressedLLB)
+        self.indexOfDatabase = "todo"
+        self.indexOfDatabase_2 = "todo"
+        self.auswahl1 = 0
+        self.auswahl2 = 0
+
+#funktionen zum laden durch drücken der buttons der datan aus der Table:
+
+    #linkers oben ladebutton
+    def pressedLLB(self):
+
+        if self.comboBox.currentText().__str__() != self.indexOfDatabase:
+            self.indexOfDatabase = str(self.comboBox.currentText())
+            self.loadDb(self.indexOfDatabase)
+        else:
+            print("nichts zu laden")
+    # rechters oben ladebutton
+    def pressedRLB(self):
+
+        if self.comboBox_2.currentText().__str__() != self.indexOfDatabase_2:
+            self.indexOfDatabase_2 = str(self.comboBox_2.currentText())
+            self.loadDb2(self.indexOfDatabase_2)
+        else:
+            print("nichts zu laden")
+
+# programm liesst daten aus der Datenbank ein:
+
+    # interieren durch for und { } um die boxen durch zugehen
+
+    # fenster links oben
+    def loadDb(self, index):
+        # Leere tabelle
+        self.tableWidget.clear()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(0)
+        # fangen an die rechte tabelle zu füllen
+        index_of_table = self.bot.zeige(index)
+        for row_number, row_data in enumerate(index_of_table):
+            self.tableWidget.insertRow(row_number)
+            for colum_number, data in enumerate(row_data):
+                print("Eingang: " + str(data))
+                self.tableWidget.setColumnCount(len(row_data))
+                if type(data) == datetime.date:
+                    data = str(data)
+                    print("datetime data hinzufügen: " + data)
+                    self.tableWidget.setItem(row_number, colum_number, QtWidgets.QTableWidgetItem(data))
+                else:
+                    print("Anderer datensatz hinzufügen: " + str(data))
+                    self.tableWidget.setItem(row_number, colum_number, QtWidgets.QTableWidgetItem(str(data)))
+
+    # fenster rechts oben
+    def loadDb2(self, index):
+        # Leere tabelle
+        self.tableWidget_2.clear()
+        self.tableWidget_2.setRowCount(0)
+        self.tableWidget_2.setColumnCount(0)
+        # fangen an die rechte tabelle zu füllen
+        index_of_table = self.bot.zeige(index)
+        for row_number, row_data in enumerate(index_of_table):
+            self.tableWidget_2.insertRow(row_number)
+            for colum_number, data in enumerate(row_data):
+                print("Eingang: " + str(data))
+                self.tableWidget_2.setColumnCount(len(row_data))
+                if type(data) == datetime.date:
+                    data = str(data)
+                    print("datetime data hinzufügen: " + data)
+                    self.tableWidget_2.setItem(row_number, colum_number, QtWidgets.QTableWidgetItem(data))
+                else:
+                    print("Anderer datensatz hinzufügen: " + str(data))
+                    self.tableWidget_2.setItem(row_number, colum_number, QtWidgets.QTableWidgetItem(str(data)))
+
+# dropdown menus:
+
+    #links oben
+    def loadDb_com(self):
+        index = self.bot.zeige_tables()
+        for i in index:
+            r = str(i)
+            r = r[2:-3]
+            self.comboBox.addItem(r)
+
+    #rechts oben
+    def loadDb2_com(self):
+        index = self.bot.zeige_tables()
+        for i in index:
+            r = str(i)
+            r = r[2: -3]
+            self.comboBox_2.addItem(r)
+
+    #unten
+    def loadDb3_com(self):
+        self.comboBox_3.addItem("Show Tables")
+        self.comboBox_3.addItem("Order By")
+        self.comboBox_3.addItem("Left Join")
+        self.comboBox_3.addItem("Right Join")
+
+
+    # funktion zum laden beim starten bzw inizaliersien
+    def start_up_load(self):
+        self.loadDb(self.indexOfDatabase)
+        self.loadDb_com()
+        self.loadDb2(self.indexOfDatabase_2)
+        self.loadDb2_com()
+        self.loadDb3_com()
+
+
+    def pressedEinfL(self):
+        self.auswahl1 = self.spinBox.value()
+        print("hier setze ich mein valu")
+        print(self.bot.get_tablename())
+        self.bot.enf(self.bot.get_tablename(), self.auswahl1)
+        print("jetzt hab ich die funktion ausgeführt ")
+
+
+    def pressedEinfR(self):
+        self.auswahl2 = self.spinBox_2.value()
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Mymain()
+    ui.setupUi(MainWindow)
+    ui.start_up_load()
+    MainWindow.show()
+    sys.exit(app.exec_())
